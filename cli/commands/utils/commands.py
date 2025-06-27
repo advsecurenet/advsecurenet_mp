7d5c8ac2a20b07fea06798d36dda5476213b78b2
fabienmorgan <fabien.morgan@criptext.com>
2025-06-27 10:23:13 +0200

    Small changes

--- cli/commands/utils/commands.py
+++ cli/commands/utils/commands.py
@@ -4,7 +4,6 @@ from cli.commands.utils.configs.commands import configs
 from cli.commands.utils.models.commands import models
 from cli.commands.utils.normalization.commands import normalization
 from cli.commands.utils.weights.commands import weights
-from cli.commands.utils.datasets.commands import datasets
 
 
 @click.group()
@@ -18,4 +17,3 @@ utils.add_command(weights)
 utils.add_command(configs)
 utils.add_command(models)
 utils.add_command(normalization)
-utils.add_command(datasets)
