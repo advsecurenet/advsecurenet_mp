bf048bd1b983a3299da405a3b432f6ec4e698dbb
fabienmorgan <fabien.morgan@criptext.com>
2025-08-12 14:23:43 +0200

    Cleand up logic in base_dataset as suggested by Melih Catal in the PR review

--- advsecurenet/datasets/base_dataset.py
+++ advsecurenet/datasets/base_dataset.py
@@ -114,7 +114,7 @@ class BaseDataset(TorchDataset, ABC):
         Returns:
             DatasetWrapper: The dataset loaded into memory.
         """
-        if "root" not in kwargs or kwargs["root"] is None:
+        if not kwargs.get("root"):
             kwargs["root"] = pkg_resources.resource_filename("advsecurenet", "data")
 
         kwargs["transform"] = self.get_transforms()
