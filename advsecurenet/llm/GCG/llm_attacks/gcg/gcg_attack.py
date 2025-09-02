bf1f730d83f5b2651a3800c922f062c5ae5c403c
Rockyph <philipchristianrocki@gmail.com>
2025-09-02 08:04:50 +0200

    Continuing to refactor the codebase, removed deprecated experiment folder

--- advsecurenet/llm/GCG/llm_attacks/gcg/gcg_attack.py
+++ advsecurenet/llm/GCG/llm_attacks/gcg/gcg_attack.py
@@ -5,8 +5,8 @@ import torch
 import torch.nn as nn
 from tqdm.auto import tqdm
 
-from llm_attacks import AttackPrompt, MultiPromptAttack, PromptManager
-from llm_attacks import get_embedding_matrix, get_embeddings
+from advsecurenet.llm.GCG.llm_attacks.base.attack_manager import AttackPrompt, MultiPromptAttack, PromptManager
+from advsecurenet.llm.GCG.llm_attacks.base.attack_manager import get_embedding_matrix, get_embeddings
 
 
 def token_gradients(model, input_ids, input_slice, target_slice, loss_slice):
