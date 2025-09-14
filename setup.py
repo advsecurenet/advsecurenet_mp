9ece9b2d4b716fadbfb9273a01f0344cb1fec1c0
fabienmorgan <fabien.morgan@criptext.com>
2025-09-14 19:38:06 +0200

    Fixed setup to include shared folder

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     author_email="melihcatal@gmail.com",
     url="https://github.com/melihcatal/advsecurenet",
     packages=find_packages(),
+    package_dir={"": "."},  
     include_package_data=True,
     package_data={"": ["*.yml"]},
     python_requires=">=3.10",
