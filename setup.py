7ba1590f0dd78c01eae66b79540635d43b6fb328
Rockyph <philipchristianrocki@gmail.com>
2025-09-15 21:09:18 +0200

    modified the setup to include necessary packages for object detection CV attacks

--- setup.py
+++ setup.py
@@ -34,15 +34,16 @@ setup(
         "einops",
         "filetype",
         "requests",
-        "transformers==4.48.3",  # Ensure compatibility with YOLOv5
+        "pydantic>=2,<3",
+        "transformers==4.48.3",
+        "fschat==0.2.36",
         "datasets",
         "mean_average_precision",
         "yolov5",
         "huggingface-hub==0.24.1",  # Ensure compatibility with YOLOv5
         "pycocotools",
         "pandas",
-        "numpy",
-        "pydantic>=2,<3",
+        "numpy",    
         "soundfile>=0.12,<0.14",  
         "safetensors>=0.4",     # commonly needed with HF models
         "accelerate>=0.33",
