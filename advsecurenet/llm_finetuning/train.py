233fd3d39fd1249c49e0827357a55ddba33bf83f
Rockyph <philipchristianrocki@gmail.com>
2025-10-16 22:27:46 +0200

    added test for cli interface for llm finetuning

--- advsecurenet/llm_finetuning/train.py
+++ advsecurenet/llm_finetuning/train.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     import pathlib
 
     # Example path to your config file
-    cfg_path = "advsecurenet_mp/advsecurenet/llm_finetuning/configs/instruction.yaml"
+    cfg_path = "/Users/philip/Desktop/advsecurenet_mp/advsecurenet/llm_finetuning/configs/instruction.yaml"
 
     cfg = load_yaml(str(cfg_path))
     run_training(cfg)
